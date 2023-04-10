8e737147245565d3120cfdedeb59f30e668d5b7b
Krzysztof R <plkrozbicki@gmail.com>
2023-04-10 17:44:02 +0200

    Test navbar

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { createProduct } from './components/product';
+import { createMenu } from './components/menu';
+
+const wrapperEL = document.getElementById('wrapper');
+const navbarEl = document.getElementById('navbar');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,ts,jsx,tsx,css,scss}'],
+  content: ['./src/**/*.{html,js,ts,jsx,tsx,css,scss}', './node_modules/flowbite/**/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
